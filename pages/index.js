75984d5f7d15b6b3fbc7c3da55a6e0958099f3fc
Tanasak Saelim <tsaelim@gmail.com>
2021-05-06 23:14:40 +0700

    แกไ้ขหน้าแรกนิดหน่อยจ้า

--- pages/index.js
+++ pages/index.js
@@ -13,11 +13,11 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          ยินดีต้อนรับ <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+         เริ่มต้นปรับปรุงกันที่นี่{' '}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
